1e4405a3aecc1d900c21f7ae0acde6c30b1da196
Robin Tegg <robin@tegg.me.uk>
2017-10-02 23:04:08 +0100

    bpm bits

--- src/main/java/com/robintegg/demo/allinjavawebapp/AllInJavaWebappApplication.java
+++ src/main/java/com/robintegg/demo/allinjavawebapp/AllInJavaWebappApplication.java
@@ -1,7 +1,15 @@
 package com.robintegg.demo.allinjavawebapp;
 
+import java.util.HashMap;
+import java.util.Map;
+
+import org.activiti.engine.RepositoryService;
+import org.activiti.engine.RuntimeService;
+import org.activiti.engine.TaskService;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 public class AllInJavaWebappApplication {
@@ -9,4 +17,22 @@ public class AllInJavaWebappApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(AllInJavaWebappApplication.class, args);
 	}
+
+	@Bean
+	CommandLineRunner init(final RepositoryService repositoryService, final RuntimeService runtimeService,
+			final TaskService taskService) {
+
+		return new CommandLineRunner() {
+
+			@Override
+			public void run(String... strings) throws Exception {
+				Map<String, Object> variables = new HashMap<String, Object>();
+				variables.put("applicantName", "John Doe");
+				variables.put("email", "john.doe@activiti.com");
+				variables.put("phoneNumber", "123456789");
+				runtimeService.startProcessInstanceByKey("hireProcess", variables);
+			}
+		};
+	}
+
 }
